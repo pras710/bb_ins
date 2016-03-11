899a01028a5729435b36d7e4cc7977f2faabab28
Haibo Zhang <huz123@philab02.cse.psu.edu>
2016-03-11 13:48:32 -0500

    recovered lightstrander from java decompiler

---
+++ old_files/LightStranderHalfDeletedAndRecovered.java
@@ -0,0 +1,183 @@
+package main.java;
+import org.apache.spark.*;
+import org.apache.spark.storage.*;
+import org.apache.spark.api.java.*;
+import org.apache.spark.api.java.function.*;
+import java.util.zip.*;
+import java.net.*;
+import java.io.*;
+import java.sql.*;
+import java.util.*;
+import java.util.concurrent.*;
+import scala.reflect.ClassTag; 
+import scala.reflect.ClassTag$;
+//class Addresses implements Comparable<String>
+//{
+//	String addresses[] = null;
+//	public Addresses(String add)
+//	{
+//		this.addresses = add.split(",");
+//	}
+//	public int compareTo(String s)
+//	{
+//	}
+//}
+public class LightStrander 
+{
+	static boolean insDecodeDebug = false;
+	static long linesRead = 0;
+
+	static long totalInstructions = 0;
+	static String toCheckString = ""; 
+	static Hashtable<String, String> tidStringTracker = new Hashtable<>();
+	public static void main1(String args[])throws Exception
+	{
+
+//		getSub("[(400d160c,13), (400d160c,13)]");
+//		getSub("[(400d160c,13), (400d160c,13), (400d160c,13)]");
+//		getSub("[(400d160c,13), (400d160c,13), (400d160c,13), (400d10c4,21), (400d160c,13), (400d160c,13)]");
+		if(args == null)
+		{
+			args = new String[]{"google_ins_dump_bbs.gz", "1", ""};
+		}
+		String path = "/i3c/hpcl/huz123/spark/java_example_pras/data_proc/";
+		System.out.println(args[2]);
+		HashSet<Long> correctedBasicBlockFlows = new HashSet<>();
+		HashMap<Long, BasicBlock> basicBlocksFormed = new HashMap<>();
+		TreeMap<Long, Map.Entry<BasicBlock, Long>> programFlow = new TreeMap<>();
+		TreeMap<Long, String> addressSequence = new TreeMap<Long, String>();
+		int upto = Integer.parseInt(args[1]);
+		toCheckString = args[2];
+		String line = "";
+		//OLD: BufferedReader br = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(args[0]))));
+		//NEW:
+		SparkConf conf = new SparkConf().setAppName("LightStrander Hero DA!");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		System.out.println("starting the file read.. with "+sc.defaultMinPartitions()+" partitions");
+		JavaRDD<String> lines_bbs = sc.textFile(path+args[0], 100).cache();//+"_bbs"
+		
+		//System.out.println("bbFlows is geting :"+lines.toArray().size());//size());
+		JavaRDD<BasicBlock> bbFlows = lines_bbs.map(new Function<String, BasicBlock>(){
+					@Override
+					public BasicBlock call(String bbString)
+					{
+						BasicBlock bb = null;
+					//	if(bbString.indexOf("Lookup") == -1)
+					//	{
+					//		System.out.println(bbString);
+					//	}
+						if(bbString.indexOf("null")!=-1 && (bbString.indexOf("Lookup") != -1 || bbString.indexOf("Lokup") != -1))
+						{
+							return new BasicBlock("gap", -1, bbString);
+						}
+						try
+						{
+							/*if(bbString.indexOf("Lookup") != -1)
+							{
+								System.out.println("lookup");
+								String[] temp_s = bbString.split(" "); 
+								temp_s[0] = temp_s[0].replaceAll(",","").trim();
+								String pc_string = "pc";
+								pc_string = temp_s[2];
+								long pc = Long.parseLong(pc_string.trim(), 16);
+								int bytes = Integer.parseInt(temp_s[3].trim());
+								bb = new BasicBlock("gap", pc, bbString);
+								return bb;
+							}
+							else*/ if(bbString.indexOf("*end*") == -1)
+							{
+								//first line..
+								return new BasicBlock("!gap", -1, bbString);
+							}
+							else
+							{
+								//System.out.println("good?");
+								StringTokenizer strTok = new StringTokenizer(bbString, "|^|");
+								String lineNumber = "";//strTok.nextToken();
+								String line = strTok.nextToken();
+								if(line.indexOf("Lookup")!=-1 || line.indexOf("Lokup")!=-1)
+								{
+									line = strTok.nextToken();
+								}
+								String prev = "";
+								String temp_s[] = line.split(" ");
+								temp_s[0] = temp_s[0].replaceAll(",", "").trim();
+								try
+								{
+									String pc_string = temp_s[1];
+									long pc = Long.parseLong(pc_string, 16);
+									bb = new BasicBlock(pc, Integer.parseInt(temp_s[0]), line.substring(4));
+									boolean endReached = false;
+									do
+									{
+										if(!strTok.hasMoreTokens())
+											break;
+										prev = line;
+										line = strTok.nextToken();
+										if(line.indexOf("Lookup") == -1 && line.indexOf("Lokup") == -1)
+											if(endReached)
+												bb.memoryLines.add(line);
+											else
+												if(line.indexOf(" ") == -1)
+												{
+													if(line.trim().length() != 0)
+													{
+														lineNumber = line;
+														endReached = true;
+													}
+												} else
+													if(line.indexOf("*end*") != -1)
+													{
+														lineNumber = prev;
+														endReached = true;
+													} else
+													{
+														try
+														{
+															bb.instructions.add(line.substring(4));
+														}
+														catch(Exception e)
+														{
+															e.printStackTrace();
+															System.out.println(line);
+															bb.fillInsAndOuts();
+															bb.printStrands();
+															throw e;
+														}
+														bb.insCount++;
+														bb.myIdStr = (new StringBuilder()).append("(").append(Long.toHexString(bb.startingPC)).append(",").append(bb.insCount).append(")").toString();
+													}
+									} while(true);
+								}
+								catch(Exception e)
+								{
+									System.out.println(line);
+									e.printStackTrace();
+									throw e;
+								}
+								try
+								{
+									bb.fillInsAndOuts();
+								}
+								catch(Exception e)
+								{
+									e.printStackTrace();
+									throw e;
+								}
+								bb.emptyIntermediates();
+								return bb;
+								Exception e;
+								e;
+								e.printStackTrace();
+								throw e;
+							}
+							//	bb.printStrands();
+		//}
+		//bbFlows.partition
+		//bbFlows.saveAsTextFile("file:/i3c/hpcl/huz123/spark/java_example_pras/output_file");
+		//bbFlows.collect().foreach(println);
+		//StrandMaintainer strand = new StrandMaintainer();
+
+	//	System.out.println(memoryFrames);
+	}
+}
--- src/main/java/LightStrander.java
+++ src/main/java/LightStrander.java
@@ -1,116 +1,191 @@
 package main.java;
+
+import java.io.*;
+import java.util.*;
 import org.apache.spark.*;
-import org.apache.spark.storage.*;
 import org.apache.spark.api.java.*;
 import org.apache.spark.api.java.function.*;
-import java.util.zip.*;
-import java.net.*;
-import java.io.*;
-import java.sql.*;
-import java.util.*;
-import java.util.concurrent.*;
-import scala.reflect.ClassTag; 
-import scala.reflect.ClassTag$;
-//class Addresses implements Comparable<String>
-//{
-//	String addresses[] = null;
-//	public Addresses(String add)
-//	{
-//		this.addresses = add.split(",");
-//	}
-//	public int compareTo(String s)
-//	{
-//	}
-//}
-public class LightStrander 
+
+public class LightStrander
 {
 	static boolean insDecodeDebug = false;
-	static long linesRead = 0;
+	static long linesRead = 0L;
+	static long totalInstructions = 0L;
+	static String toCheckString = "";
+	static Hashtable<String, String> tidStringTracker = new Hashtable();
 
-	static long totalInstructions = 0;
-	static String toCheckString = ""; 
-	static Hashtable<String, String> tidStringTracker = new Hashtable<>();
-	public static void main1(String args[])throws Exception
+	public static void main1(String[] args)	throws Exception
 	{
-
-//		getSub("[(400d160c,13), (400d160c,13)]");
-//		getSub("[(400d160c,13), (400d160c,13), (400d160c,13)]");
-//		getSub("[(400d160c,13), (400d160c,13), (400d160c,13), (400d10c4,21), (400d160c,13), (400d160c,13)]");
-		if(args == null)
-		{
-			args = new String[]{"google_ins_dump_bbs.gz", "1", ""};
+		if (args == null) {
+			args = new String[] { "google_ins_dump_bbs.gz", "1", "" };
 		}
 		String path = "/i3c/hpcl/huz123/spark/java_example_pras/data_proc/";
 		System.out.println(args[2]);
-		HashSet<Long> correctedBasicBlockFlows = new HashSet<>();
-		HashMap<Long, BasicBlock> basicBlocksFormed = new HashMap<>();
-		TreeMap<Long, Map.Entry<BasicBlock, Long>> programFlow = new TreeMap<>();
-		TreeMap<Long, String> addressSequence = new TreeMap<Long, String>();
+		HashSet<Long> correctedBasicBlockFlows = new HashSet();
+		HashMap<Long, BasicBlock> basicBlocksFormed = new HashMap();
+		TreeMap<Long, Map.Entry<BasicBlock, Long>> programFlow = new TreeMap();
+		TreeMap<Long, String> addressSequence = new TreeMap();
 		int upto = Integer.parseInt(args[1]);
 		toCheckString = args[2];
 		String line = "";
-		//OLD: BufferedReader br = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(args[0]))));
-		//NEW:
+
 		SparkConf conf = new SparkConf().setAppName("LightStrander Hero DA!");
 		JavaSparkContext sc = new JavaSparkContext(conf);
-		System.out.println("starting the file read.. with "+sc.defaultMinPartitions()+" partitions");
-		JavaRDD<String> lines_bbs = sc.textFile(path+args[0], 100).cache();//+"_bbs"
-		
-		//System.out.println("bbFlows is geting :"+lines.toArray().size());//size());
+		System.out.println("starting the file read.. with " + sc.defaultMinPartitions() + " partitions");
+		JavaRDD<String> lines_bbs = sc.textFile(path + args[0], 100).cache();
+
 		JavaRDD<BasicBlock> bbFlows = lines_bbs.map(new Function<String, BasicBlock>(){
-					@Override
-					public BasicBlock call(String bbString)
+				@Override
+				public BasicBlock call(String bbString)
+				{
+					BasicBlock bb = null;
+					if ((bbString.indexOf("null") != -1) && ((bbString.indexOf("Lookup") != -1) || (bbString.indexOf("Lokup") != -1))) {
+						return new BasicBlock("gap", -1L, bbString);
+					}
+					try
 					{
-						BasicBlock bb = null;
-					//	if(bbString.indexOf("Lookup") == -1)
-					//	{
-					//		System.out.println(bbString);
-					//	}
-						if(bbString.indexOf("null")!=-1 && (bbString.indexOf("Lookup") != -1 || bbString.indexOf("Lokup") != -1))
+						if (bbString.indexOf("*end*") == -1) {
+							return new BasicBlock("!gap", -1L, bbString);
+						}
+						StringTokenizer strTok = new StringTokenizer(bbString, "|^|");
+						String lineNumber = "";
+						String line = strTok.nextToken();
+						if ((line.indexOf("Lookup") != -1) || (line.indexOf("Lokup") != -1)) {
+							line = strTok.nextToken();
+						}
+						String prev = "";
+						String[] temp_s = line.split(" ");
+						temp_s[0] = temp_s[0].replaceAll(",", "").trim();
+						try
 						{
-							return new BasicBlock("gap", -1, bbString);
+							String pc_string = temp_s[1];
+							long pc = Long.parseLong(pc_string, 16);
+							bb = new BasicBlock(pc, Integer.parseInt(temp_s[0]), line.substring(4));
+							boolean endReached = false;
+							while (strTok.hasMoreTokens())
+							{
+								prev = line;
+								line = strTok.nextToken();
+								if ((line.indexOf("Lookup") == -1) && (line.indexOf("Lokup") == -1)) {
+									if (endReached)
+									{
+										bb.memoryLines.add(line);
+									}
+									else if (line.indexOf(" ") == -1)
+									{
+										if (line.trim().length() != 0)
+										{
+											lineNumber = line;
+											endReached = true;
+										}
+									}
+									else if (line.indexOf("*end*") != -1)
+									{
+										lineNumber = prev;
+										endReached = true;
+									}
+									else
+									{
+										try
+										{
+											bb.instructions.add(line.substring(4));
+										}
+										catch (Exception e)
+										{
+											e.printStackTrace();
+											System.out.println(line);
+											bb.fillInsAndOuts();
+											bb.printStrands();
+											throw e;
+										}
+										bb.insCount += 1;
+										bb.myIdStr = ("(" + Long.toHexString(bb.startingPC) + "," + bb.insCount + ")");
+									}
+								}
+							}
+						}
+						catch (Exception e)
+						{
+							System.out.println(line);
+							e.printStackTrace();
+							throw e;
 						}
 						try
 						{
-							/*if(bbString.indexOf("Lookup") != -1)
-							{
-								System.out.println("lookup");
-								String[] temp_s = bbString.split(" "); 
-								temp_s[0] = temp_s[0].replaceAll(",","").trim();
-								String pc_string = "pc";
-								pc_string = temp_s[2];
-								long pc = Long.parseLong(pc_string.trim(), 16);
-								int bytes = Integer.parseInt(temp_s[3].trim());
-								bb = new BasicBlock("gap", pc, bbString);
-								return bb;
-							}
-							else*/ if(bbString.indexOf("*end*") == -1)
-							{
-								//first line..
-								return new BasicBlock("!gap", -1, bbString);
-							}
-							else
-							{
-								//System.out.println("good?");
-								StringTokenizer strTok = new StringTokenizer(bbString, "|^|");
-								String lineNumber = "";//strTok.nextToken();
-								String line = strTok.nextToken();
-								if(line.indexOf("Lookup")!=-1 || line.indexOf("Lokup")!=-1)
-								{
-									line = strTok.nextToken();
-								}
-								String prev = "";
-								String temp_s[] = line.split(" ");
-???LINES MISSING
-???LINES MISSING
-???LINES MISSING
-		//	bb.printStrands();
-		//}
-		//bbFlows.partition
-		//bbFlows.saveAsTextFile("file:/i3c/hpcl/huz123/spark/java_example_pras/output_file");
-		//bbFlows.collect().foreach(println);
-		//StrandMaintainer strand = new StrandMaintainer();
+							bb.fillInsAndOuts();
+						}
+						catch (Exception e)
+						{
+							e.printStackTrace();
+							throw e;
+						}
+						bb.emptyIntermediates();
+						return bb;
+					}
+					catch (Exception e)
+					{
+						e.printStackTrace();
+						throw e;
+					}
+				}
+			});
+		lines_bbs = null;
+		System.out.println("done with the bb reads!!");
 
-	//	System.out.println(memoryFrames);
+		JavaRDD<String> strMaints = bbFlows.mapPartitions(new FlatMapFunction<Iterator<BasicBlock>, String>()
+				{
+					@Override
+					public Iterable<String> call(Iterator<BasicBlock> biter)
+					{
+						StrandMaintainer strm = new StrandMaintainer(-1L);
+						while (biter.hasNext())
+						{
+							BasicBlock b = (BasicBlock)biter.next();
+							if (!b.isGap) {
+								strm.addNext(b, -1L);
+							}
+						}
+						return strm.myFinalStrands;
+					}
+				});
+		TreeMap<String, HashMap<String, Integer>> myPopularStrands = new TreeMap();
+		Iterator<String> iterS = strMaints.toLocalIterator();
+		while (iterS.hasNext())
+		{
+			String s = (String)iterS.next();
+			myPopularStrands.put(s, new HashMap());
+		}
+		strMaints = null;
+
+		StrandDataBased dataKeeper = new StrandDataBased(myPopularStrands);
+		StrandDataBased dataSpew = (StrandDataBased)bbFlows.treeAggregate(dataKeeper, new Function2<StrandDataBased, BasicBlock, StrandDataBased>()
+			{
+				@Override
+				public StrandDataBased call(StrandDataBased l, BasicBlock b)
+				{
+					if (b.isGap) {
+						return l;
+					}
+					l.addNext(b);
+					b.emptyIntermediates();
+					return l;
+				}}, 
+				new Function2<StrandDataBased, StrandDataBased, StrandDataBased>(){
+					@Override
+					public StrandDataBased call(StrandDataBased a, StrandDataBased b)
+					{
+						for (String s : b.topStrands.keySet())
+						{
+							System.out.println(s + ":");
+							System.out.println(b.topStrands.get(s));
+						}
+						b.topStrands.clear();
+						a.addAllChains(b);
+						b.clearAll();
+						return a;
+					}
+				});
+		System.out.println("***********************");
+		System.out.println("***********************");
 	}
 }
